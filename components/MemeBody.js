768467d72892811c5d2d82ce05c52aa927f950a4
Jarek Hryniewicki <jarhryniewicki@gmail.com>
2022-01-16 13:49:29 +0100

    Handle Image onError

--- components/MemeBody.js
+++ components/MemeBody.js
@@ -1,18 +1,23 @@
 import Image from 'next/image';
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
 const MemeBody = ({ topTitle, image, text, bottomTitle }) => {
+  const [imageSrc, setImageSrc] = useState(image)
+  const handleImageError = () => {
+    setImageSrc('/default-image.png')
+  };
   return (
     <div className="bg-gray-800 mx-auto w-11/12 overflow-auto">
       <h2 className="text-center text-2xl my-6 font-bold">{topTitle}</h2>
       <div className="mx-auto w-11/12">
         <Image
-          src={`${image}`}
+          src={`${imageSrc}`}
           alt={`${topTitle} ${bottomTitle}`}
           width={1000}
           height={1000}
           objectFit="contain"
+          onError={handleImageError}
         />
       </div>
       <h2 className="text-center text-2xl my-6 font-bold">{bottomTitle}</h2>
